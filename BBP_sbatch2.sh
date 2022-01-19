54ed69ef90036067e56530d11e34edbdca98bd1e
Roy Ben-Shalom <bens.roy@gmail.com>
2022-01-18 21:05:30 -0800

    adding files 	new file:   BBP_sbatch2.sh 	new file:   nrn_helper.py 	new file:   testcell.csv

---
+++ BBP_sbatch2.sh
@@ -0,0 +1,121 @@
+#!/bin/bash -l
+#SBATCH -N 1
+#SBATCH -t 30:00
+#SBATCH -q debug
+#SBATCH -J DL4N_full_prod
+#SBATCH -L SCRATCH,cfs
+#SBATCH -C knl
+#SBATCH --output logs/%A_%a  # job-array encodding
+#SBATCH --image=balewski/ubu20-neuron8:v3
+#SBATCH --array 1-1
+
+# Stuff for knl
+export OMP_NUM_THREADS=1
+module unload craype-hugepages2M
+
+# All paths relative to this, prepend this for full path name
+#WORKING_DIR=/global/cscratch1/sd/adisaran/DL4neurons
+#OUT_DIR=/global/cfs/cdirs/m2043/adisaran/wrk/
+OUT_DIR=/global/cscratch1/sd/roybens/bbpexcv3/wrk/
+# simu run in the dir where  Slurm job was started
+
+CELLS_FILE='excitatorycells.csv'
+START_CELL=0
+NCELLS=1
+END_CELL=$((${START_CELL}+${NCELLS}))
+NSAMPLES=1
+NRUNS=1
+NSAMPLES_PER_RUN=$(($NSAMPLES/$NRUNS))
+
+echo "CELLS_FILE" ${CELLS_FILE}
+echo "START_CELL" ${START_CELL}
+echo "NCELLS" ${NCELLS}
+echo "END_CELL" ${END_CELL}
+
+export THREADS_PER_NODE=1
+
+# to prevent: H5-write error: unable to lock file, errno = 524
+export HDF5_USE_FILE_LOCKING=FALSE
+
+# Create all outdirs
+echo "Making outdirs at" `date`
+arrIdx=${SLURM_ARRAY_TASK_ID}
+jobId=${SLURM_ARRAY_JOB_ID}_${arrIdx}
+RUNDIR=${OUT_DIR}/runs2/${jobId}
+mkdir -p $RUNDIR
+for i in $(seq $((${START_CELL}+1)) ${END_CELL});
+do
+    line=$(head -$i ${CELLS_FILE} | tail -1)
+    bbp_name=$(echo $line | awk -F "," '{print $1}')
+    for k in {1..1}
+    do
+        mkdir -p $RUNDIR/$bbp_name/c${k}
+        chmod a+rx $RUNDIR/$bbp_name/c${k}
+    done
+done
+cp BBP_sbatch2.sh $RUNDIR
+chmod a+rx $RUNDIR
+chmod a+rx $RUNDIR/*
+echo done
+date
+
+echo "Done making outdirs at" `date`
+
+export stimname1=chaotic_2
+export stimname2=step
+export stimname3=ramp
+export stimname4=chirp
+export stimname5=sine
+
+stimfile1=stims/${stimname1}.csv
+stimfile2=stims/${stimname2}.csv
+stimfile3=stims/${stimname3}.csv
+stimfile4=stims/${stimname4}.csv
+stimfile5=stims/${stimname5}.csv
+echo
+env | grep SLURM
+echo
+
+
+FILENAME=\{BBP_NAME\}-v3
+echo "STIM FILE" $stimfile
+echo "SLURM_NODEID" ${SLURM_NODEID}
+echo "SLURM_PROCID" ${SLURM_PROCID}
+
+REMOTE_CELLS_FILE='/tmp/excitatorycells.csv'
+#sbcast ${CELLS_FILE} ${REMOTE_CELLS_FILE}
+REMOTE_CELLS_FILE=${CELLS_FILE}
+echo REMOTE_CELLS_FILE $REMOTE_CELLS_FILE
+#( sleep 180; echo `hostname` ; date; free -g; top ibn1) >&L1&
+#( sleep 260; echo `hostname` ; date; free -g; top ibn1) >&L2&
+#( sleep 400; echo `hostname` ; date; free -g; top ibn1) >&L3&
+
+for j in $(seq 1 ${NRUNS});
+do
+    echo "Doing run $j of $NRUNS at" `date`
+    for l in {0..0}
+    do
+        adjustedval=$((l+1))
+        METADATA_FILE=$RUNDIR/${FILENAME}-meta-${adjustedval}.yaml
+        OUTFILE=$RUNDIR/\{BBP_NAME\}/c${adjustedval}/${FILENAME}-\{NODEID\}-$j-c${adjustedval}.h5
+	
+        args="--outfile $OUTFILE --stim-file ${stimfile1} ${stimfile2} ${stimfile3} ${stimfile4} ${stimfile5} --model BBP --cell-i ${l} \
+          --cori-csv ${REMOTE_CELLS_FILE} --cori-start ${START_CELL} --cori-end ${END_CELL} \
+          --num ${NSAMPLES_PER_RUN} --trivial-parallel --print-every 8 --linear-params-inds 12 17 18\
+          --metadata-file ${METADATA_FILE} --stim-noise --dt 0.1"
+        echo "args" $args
+        srun --input none -k -n $((${SLURM_NNODES}*${THREADS_PER_NODE})) --ntasks-per-node ${THREADS_PER_NODE} shifter python3  run.py $args
+
+        # Write one metadata yaml per cell
+        srun -n ${NCELLS} shifter python3 run.py $args --metadata-only
+    done
+    
+    chmod -R a+r $RUNDIR/*.yaml
+    # run.py sets permissions on the data files themselves (doing them here simultaneously takes forever)
+    
+    echo "Done run $j of $NRUNS at" `date`
+
+done
+
+
+#salloc -q debug -C knl --image=balewski/ubu20-neuron8:v2 -t 30:00 -N 1
---
+++ nrn_helper.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import numpy as np
+plt.rcParams['axes.spines.right'] = False
+plt.rcParams['axes.spines.top'] = False
+plt.rcParams['font.sans-serif'] = "Arial"
+plt.rcParams['font.family'] = "sans-serif"
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+
+tick_major = 6
+tick_minor = 4
+plt.rcParams["xtick.major.size"] = tick_major
+plt.rcParams["xtick.minor.size"] = tick_minor
+plt.rcParams["ytick.major.size"] = tick_major
+plt.rcParams["ytick.minor.size"] = tick_minor
+
+font_small = 12
+font_medium = 13
+font_large = 14
+plt.rc('font', size=font_small)          # controls default text sizes
+plt.rc('axes', titlesize=font_medium)    # fontsize of the axes title
+plt.rc('axes', labelsize=font_medium)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=font_small)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=font_small)    # fontsize of the tick labels
+plt.rc('legend', fontsize=font_small)    # legend fontsize
+plt.rc('figure', titlesize=font_large)   # fontsize of the figure title
+
+stim_folder = './stims/'
+def plot_stim(stim_fn,dt=0.1):
+    stim = np.genfromtxt(stim_fn, dtype=np.float32)
+    time_pts = np.ones(len(stim))*dt
+    time_pts = np.cumsum(time_pts)
+    fig, ax= plt.subplots(1,figsize=(5,5))
+    ax.plot(time_pts,stim)
+    ax.set_title(f'{stim_fn}')
